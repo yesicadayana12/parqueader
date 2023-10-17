1656a2f9e4601fc2971ac98693b382dd90bcde72
Yesica Beltran <guzman.carvajal240406@gmail.com>
2023-10-17 17:38:18 -0500

    incluyendo areglos en el parqueadero primera parte

--- src/parq.java
+++ src/parq.java
@@ -9,30 +9,33 @@ public class parq {
         double hora_salida;
         double valor, dato = 1;
         double resta, mult;
+        int carro = 10,moto = 10;
 
         JOptionPane.showMessageDialog(null, "======================================= \n" +
                 "bienvenido al parqueadero V \n" +
                 "el costo por hora\n" +
                 "carro: 2000\n" + "moto: 1000\n" +
                 "=================================================");
-        nombre = JOptionPane.showInputDialog("porfavor ingrese su nombre: ");
+
         placa = JOptionPane.showInputDialog("porfavor ingrese su placa");
         hora_entrada = Double.parseDouble(JOptionPane.showInputDialog("porfavor ingrese la hora en la que ingreso"));
         hora_salida = Double.parseDouble(JOptionPane.showInputDialog("PORFAVOR INGRESE LA HORA EN QUE SALIO "));
-        valor = Double.parseDouble(JOptionPane.showInputDialog("Porfavor escriba 1 si su automuvil es un carro pero si no escriba 2 si es moto"));
-        resta = hora_salida - hora_entrada;
+        valor= Double.parseDouble(JOptionPane.showInputDialog(" Porfavor escriba 1 si su automovil es carro  pero si no 2 si es moto "));
+                resta = hora_salida - hora_entrada;
 
         if (valor == dato) {
-            mult = Math.ceil(resta) * valor * 2000;
+               mult = Math.ceil(resta) * valor * 2000;
         } else {
             mult = Math.ceil(resta) * valor * 1000;
         }
 
-        JOptionPane.showMessageDialog(null, "su nombre es:" + nombre + "\n" +
+        JOptionPane.showMessageDialog(null, "su carro es:" + carro + "\n" +
                 "su placa es:" + placa + "\n" +
                 "la hora en que ingreso es:" + hora_entrada + "\n" +
                 "la hora en que salio es :" + hora_salida + "\n" +
                 "tiene que pagar :" + mult);
+
+
         switch ((int) valor) {
             case 1:
                 mult = Math.ceil(resta) * 2000;
