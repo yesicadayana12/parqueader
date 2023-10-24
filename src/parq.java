afa283a449ca95f455679d0e166fd09fa04cb4ae
Yesica Beltran <guzman.carvajal240406@gmail.com>
2023-10-24 17:31:39 -0500

    incluyendo areglos en el parqueadero primera parte

--- src/parq.java
+++ src/parq.java
@@ -3,13 +3,12 @@ import javax.swing.*;
 public class parq {
     public static void main(String[] args) {
 
-        String nombre;
-        String placa;
-        double hora_entrada;
-        double hora_salida;
+        int placas;
         double valor, dato = 1;
         double resta, mult;
-        int carro = 10,moto = 10;
+        int carro = 10, moto = 10;
+        int item;
+        int dat = 1;
 
         JOptionPane.showMessageDialog(null, "======================================= \n" +
                 "bienvenido al parqueadero V \n" +
@@ -17,22 +16,45 @@ public class parq {
                 "carro: 2000\n" + "moto: 1000\n" +
                 "=================================================");
 
-        placa = JOptionPane.showInputDialog("porfavor ingrese su placa");
-        hora_entrada = Double.parseDouble(JOptionPane.showInputDialog("porfavor ingrese la hora en la que ingreso"));
-        hora_salida = Double.parseDouble(JOptionPane.showInputDialog("PORFAVOR INGRESE LA HORA EN QUE SALIO "));
-        valor= Double.parseDouble(JOptionPane.showInputDialog(" Porfavor escriba 1 si su automovil es carro  pero si no 2 si es moto "));
-                resta = hora_salida - hora_entrada;
+        item = Integer.parseInt(JOptionPane.showInputDialog(null, "1-desea ingresar  carro  \n" +
+                "2-desea ingresar moto \n" + "3-salir \n"));
 
+        switch ((int) item) {
+            case 1:
+                int[] carros = new int[3];
+            {
+                for (int i = 0; i < 3; i++) ;
+                JOptionPane.showMessageDialog(null, "Porfavor ingresa la placa de los carros ");
+
+                int[] placa = new int[3];
+                for (int i = 0; i < 3; i++) ;
+                Double.parseDouble(JOptionPane.showInputDialog(null, "Porfavor ingrese la hora de entrada"));
+
+                Double[] hora_entrada = new Double[3];
+                for (int i = 0; i < 3; i++) ;
+                Double.parseDouble(JOptionPane.showInputDialog(null, "Porfavor ingrese la hora de salida"));
+                Double[] hora_salida = new Double[3];
+                for (int i = 0; i < 3; i++) ;
+            }
+            break;
+
+            case 2:
+
+
+
+
+
+/*
+        resta = hora_salidad - hora_entrada;
         if (valor == dato) {
-               mult = Math.ceil(resta) * valor * 2000;
+            mult = Math.ceil(resta) * valor * 2000;
         } else {
             mult = Math.ceil(resta) * valor * 1000;
         }
 
-        JOptionPane.showMessageDialog(null, "su carro es:" + carro + "\n" +
-                "su placa es:" + placa + "\n" +
+        JOptionPane.showMessageDialog(null, "su placa es:" + placas + "\n" +
                 "la hora en que ingreso es:" + hora_entrada + "\n" +
-                "la hora en que salio es :" + hora_salida + "\n" +
+                "la hora en que salio es :" + hora_salidad + "\n" +
                 "tiene que pagar :" + mult);
 
 
@@ -45,12 +67,8 @@ public class parq {
 
                 JOptionPane.showMessageDialog(null, "valor a pagar por su moto es:" + mult);
 
-                defauld :
-                JOptionPane.showMessageDialog(null, "el valor no existe");
+                defauld:
+                 JOptionPane.showMessageDialog(null, "el valor no existe");
 
-        }
-    }
-}
-
-
-
+   }   }
+ */
